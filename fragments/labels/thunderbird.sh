3d21273552807a53301c6b621f827d59c7b7d94a
Maarten Wijnants <wijnants.maarten@gmail.com>
2024-12-13 12:00:59 +0100

    Update thunderbird.sh appNewVersion

--- fragments/labels/thunderbird.sh
+++ fragments/labels/thunderbird.sh
@@ -2,6 +2,6 @@ thunderbird)
     name="Thunderbird"
     type="dmg"
     downloadURL="https://download.mozilla.org/?product=thunderbird-latest&os=osx&lang=en-US"
-    appNewVersion=$(curl -fsL "https://www.thunderbird.net/notes/" | grep -m 1 -o "Version [0-9\.]*" | awk '{print $2}')
+    appNewVersion=$(curl -fsL "https://www.thunderbird.net/en-US/thunderbird/releases/atom.xml" | grep -v "esr" | grep -m 5 "<title type=" | sed 's/<.*>\(.*\)<.*>/\1/' | awk '{ print $2 }' | awk '{$1=$1};1' | sort -V | tail -n1)
     expectedTeamID="43AQ936H96"
     ;;

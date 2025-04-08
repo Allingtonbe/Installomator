3288ceb936feabfbf9cacbd8a8107d8eebb1deb1
Maarten Wijnants <wijnants.maarten@gmail.com>
2025-04-08 09:56:18 +0200

    Update microsoftoutlook.sh

--- fragments/labels/microsoftoutlook.sh
+++ fragments/labels/microsoftoutlook.sh
@@ -1,9 +1,9 @@
 microsoftoutlook)
     name="Microsoft Outlook"
     type="pkg"
-    downloadURL=$(curl -fsL "https://learn.microsoft.com/en-us/officeupdates/update-history-office-for-mac" | grep -Eo "https:\/\/officecdn.microsoft.com\/pr\/.*\/MacAutoupdate\/Microsoft_Outlook_.*_Updater.pkg" | head -1)
+    downloadURL="https://go.microsoft.com/fwlink/?linkid=525137"
     versionKey="CFBundleVersion"
-    appNewVersion=$(echo $downloadURL | cut -d "/" -f 7 | cut -d "_" -f 3)
+    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/Microsoft_.*pkg" | cut -d "_" -f 3)
     expectedTeamID="UBF8T346G9"
     if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
         printlog "Running msupdate --list"

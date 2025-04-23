c7c1c04a4666ddbf7605bf657bcd0f31dd4ea542
Maarten Wijnants <wijnants.maarten@gmail.com>
2025-04-23 14:23:13 +0200

    Update microsoftteamsnew.sh

--- fragments/labels/microsoftteamsnew.sh
+++ fragments/labels/microsoftteamsnew.sh
@@ -2,7 +2,7 @@ microsoftteamsnew)
     name="Microsoft Teams"
     type="pkg"
     downloadURL=$(curl -fsL "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409TEAMS21.xml" | xmllint --xpath '//array/dict[1]/key[text()="Location"]/following-sibling::string[1]/text()' - 2>/dev/null )
-    appNewVersion=$(curl -fsL "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409TEAMS21.xml" | xmllint --xpath '//array/dict[1]/key[text()="Title"]/following-sibling::string[1]/text()' - | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null)
+    appNewVersion=$(curl -fsL "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409TEAMS21.xml" | xmllint --xpath '//array/dict[1]/key[text()="Title"]/following-sibling::string[1]/text()' - | grep -oE '[\.0-9]+' 2>/dev/null)
     expectedTeamID="UBF8T346G9"
     blockingProcesses=( MSTeams "Microsoft Teams" "Microsoft Teams WebView" "Microsoft Teams Launcher" "Microsoft Teams (work preview)")
     if [[ -x "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/MacOS/msupdate" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
